3e4248e057ee8d21c6071a25e618d08f7faa91bc
jonghyunhub <chuwhdgus@gmail.com>
2023-07-31 20:33:37 +0900

    feat : 상품 주문번호를 통한 생성 로직 추가

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -21,10 +23,29 @@ public class ProductService {
     // 001 002 003 004
     // DB에서 마지막 저장된 Product의 상품 번호를 읽어와서 + 1
     // ex) 009(기존 마지막 상품번호) -> 010 (새로 생성한 상품번호)
-    public void createProduct(ProductCreateRequest request) {
-        String latestProductNumber = productRepository.findLatestProductNumber();
+    // 동시성 이슈도 생길 수 있음
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        String nextProductNumber = createNextProductNumber();
 
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
     }
+
+    private String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+    }
+
 
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
