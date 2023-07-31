c4216de88e7a9288b8bf86bbeadacd800721121c
jonghyunhub <chuwhdgus@gmail.com>
2023-07-31 18:31:54 +0900

    feat : createOrder 메서드 리팩토링

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.Order;
@@ -13,12 +14,14 @@ import sample.cafekiosk.spring.domain.stock.Stock;
 import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class OrderService {
 
     private final ProductRepository productRepository;
@@ -28,42 +31,22 @@ public class OrderService {
     /**
      * 주문 생성 로직
      * 1. 주문 상품 번호로 상품을 조회한다.
-     *  1-1. 상품 번호는 중복이 될 수 있다. -> findProductsBy() 에서 중복된 상품 번호로 중복된 상품 조회
+     * 1-1. 상품 번호는 중복이 될 수 있다. -> findProductsBy() 에서 중복된 상품 번호로 중복된 상품 조회
      * 2. 주문하고자 하는 상품에는 재고가 있는 상품이 존재하기 때문에 재고가 있는 상품인지 체크한다.
-     *  2-1. 재고가 있는 상품의 상품 번호로 재고 정보를 가져온다.(Stock)
-     *  2-2. 재고 리스트를 순회하면 성능이 안나오므로 Map으로 만든다. Map<주문상품번호, 재고>
+     * 2-1. 재고가 있는 상품의 상품 번호로 재고 정보를 가져온다.(Stock)
+     * 2-2. 재고 리스트를 순회하면 성능이 안나오므로 Map으로 만든다. Map<주문상품번호, 재고>
      * 3. 재고가 있는 상품의 리스트를 Map으로 변환해서 각 상품의 갯수를 집계한다.
      * 4. 재고를 차감한다.
      */
+    /**
+     * 재고 감소 -> 동시성 고민
+     * optimistic lock / pessimistic lock / ...
+     */
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
-        List<Product> products = findProductsBy(productNumbers); //1-1
+        List<Product> products = findProductsBy(productNumbers); //(1-1)
 
-        // 재고 차감 체크가 필요한 상품들 filter 2
-        List<String> stockProductNumbers = products.stream()
-                .filter(product -> ProductType.containsStockType(product.getType()))
-                .map(Product::getProductNumber)
-                .collect(Collectors.toList());
-
-        // 재고 엔티티 조회 2-1
-        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
-        Map<String, Stock> stockMap = stocks.stream()
-                .collect(Collectors.toMap(Stock::getProductNumber, s -> s)); // List를 순회하면 성능이 안나오므로 map으로 변환 2-2 Map<주문상품번호, 재고>
-
-        // 상품별 counting 3 Map<주문상품,
-        Map<String, Long> productCountingMap = stockProductNumbers.stream()
-                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
-
-        // 재고 차감 시도
-        for (String stockProductNumber : stockProductNumbers) {
-            Stock stock = stockMap.get(stockProductNumber);
-            int quantity = productCountingMap.get(stockProductNumber).intValue();
-            if (stock.isQuantityLessThan(quantity)) {
-                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
-            }
-            stock.deductQuantity(quantity);
-        }
-
+        deductStockQuantities(products);
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
@@ -89,5 +72,45 @@ public class OrderService {
         //  {"001" : 아메리카노객체},
         //  {"002" : 라떼객체}
         // }
+    }
+
+    private void deductStockQuantities(List<Product> products) {
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+        Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+        // 재고 차감 시도
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+            if (stock.isQuantityLessThan(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        // 재고 차감 체크가 필요한 상품들 filter (2)
+        return products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .collect(Collectors.toList());
+    }
+
+    private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+        // 재고 엔티티 조회 (2-1)
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        return stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+    }
+
+
+    private static Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+        // 상품별 counting (3) Map<주문상품번호,주문상품번호 갯수>
+        return stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -1,11 +1,11 @@
 package sample.cafekiosk.spring.api.service.order;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
@@ -20,13 +20,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.SELLING;
 import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
-@Transactional
 class OrderServiceTest {
     @Autowired
     private OrderService orderService;
@@ -43,12 +43,13 @@ class OrderServiceTest {
     @Autowired
     private OrderProductRepository orderProductRepository;
 
-//    @AfterEach
-//    void tearDown() {
-//        productRepository.deleteAllInBatch();
-//        orderRepository.deleteAllInBatch();
-//        orderProductRepository.deleteAllInBatch();
-//    }
+    @AfterEach
+    void tearDown() {
+        productRepository.deleteAllInBatch();
+        orderRepository.deleteAllInBatch();
+        orderProductRepository.deleteAllInBatch();
+        stockRepository.deleteAllInBatch();
+    }
 
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
@@ -84,6 +85,8 @@ class OrderServiceTest {
     @Test
     void createOrderWithStock() {
         //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         Product product1 = createProduct(BOTTLE, "001", 1000);
         Product product2 = createProduct(BAKERY, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
@@ -98,7 +101,6 @@ class OrderServiceTest {
                 .build();
 
         //when
-        LocalDateTime registeredDateTime = LocalDateTime.now();
         OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
 
         //then
@@ -122,6 +124,32 @@ class OrderServiceTest {
                         tuple("001", 0),
                         tuple("002", 1)
                 );
+    }
+
+    @DisplayName("재고가 부족한 상품으로 주문을 생성하려는 경우 예외가 발생한다.")
+    @Test
+    void createOrderWithNoStock() {
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 1);
+        Stock stock2 = Stock.create("002", 1);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        //when //then
+        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고가 부족한 상품이 있습니다.");
 
     }
 
@@ -129,6 +157,8 @@ class OrderServiceTest {
     @Test
     void createOrderWithDulpicateProductNumbers() {
         //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         Product product1 = createProduct(HANDMADE, "001", 1000);
         Product product2 = createProduct(HANDMADE, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
@@ -139,7 +169,6 @@ class OrderServiceTest {
                 .build();
 
         //when
-        LocalDateTime registeredDateTime = LocalDateTime.now();
         OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
 
         //then
@@ -158,7 +187,7 @@ class OrderServiceTest {
 
     private Product createProduct(ProductType type, String productNumber, int price) {
         return Product.builder()
-                .type(HANDMADE)
+                .type(type)
                 .productNumber(productNumber)
                 .price(price)
                 .sellingStatus(SELLING)
