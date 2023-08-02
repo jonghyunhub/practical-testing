830a9e7283d9def65ed9dbeefbbbd5f5911f4bce
jonghyunhub <chuwhdgus@gmail.com>
2023-08-02 18:52:04 +0900

    test : 신규 상품을 등록 api controller 테스트 작성

--- src/main/java/sample/cafekiosk/spring/CafeKioskApplication.java
+++ src/main/java/sample/cafekiosk/spring/CafeKioskApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class CafeKioskApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CafeKioskApplication.class, args);
     }
-
 }
