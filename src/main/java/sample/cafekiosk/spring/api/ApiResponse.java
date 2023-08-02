cc6c64e27ffe9069e4ddedf4d2cb7e5a2001388c
jonghyunhub <chuwhdgus@gmail.com>
2023-08-02 20:44:28 +0900

    test : ProductController Valid 예외상황 테스트 작성

--- src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
+++ src/main/java/sample/cafekiosk/spring/api/ApiResponse.java
@@ -1,8 +1,11 @@
 package sample.cafekiosk.spring.api;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
-import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 
+@Getter
+@NoArgsConstructor
 public class ApiResponse<T> {
     private int code;
     private HttpStatus status;
@@ -16,11 +19,15 @@ public class ApiResponse<T> {
         this.data = data;
     }
 
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus,String message ,T data) {
+        return new ApiResponse<>(httpStatus, message, data);
+    }
+
     public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
-        return new ApiResponse<>(httpStatus, httpStatus.name(), data);
+        return of(httpStatus, httpStatus.name(), data);
     }
 
     public static <T> ApiResponse<T> ok(T data) {
-        return new ApiResponse<>(HttpStatus.OK, HttpStatus.OK.name(), data);
+        return of(HttpStatus.OK, data);
     }
 }
--- src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
+++ src/test/java/sample/cafekiosk/spring/api/controller/product/ProductControllerTest.java
@@ -15,6 +15,7 @@ import sample.cafekiosk.spring.domain.product.ProductType;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ProductController.class)
@@ -49,5 +50,107 @@ class ProductControllerTest {
                 )
                 .andDo(print()) // 요청에 대한 로그를 더 자세하게 확인 가능
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("신규 상품을 등록할 때 상품 타입은 필수값이다.")
+    @Test
+    void createProductWithOutType() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        // when // then
+        // PostMapping의 경우 바디에 데이터를 넣어서 보내는데 이경우 JSON 형태로 보내므로 request 객체를 직렬화/역직렬화를 도와주는 ObjectMapper가 필요함.
+        mockMvc.perform(
+                        post("/api/v1/products/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print()) // 요청에 대한 로그를 더 자세하게 확인 가능
+                .andExpect(status().isBadRequest()) //Status 상태코드 검증
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("상품 타입은 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+
+    @DisplayName("신규 상품을 등록할 때 상품 판매상태는 필수값이다.")
+    @Test
+    void createProductWithOutSellingStatus() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(ProductType.HANDMADE)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        // when // then
+        // PostMapping의 경우 바디에 데이터를 넣어서 보내는데 이경우 JSON 형태로 보내므로 request 객체를 직렬화/역직렬화를 도와주는 ObjectMapper가 필요함.
+        mockMvc.perform(
+                        post("/api/v1/products/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print()) // 요청에 대한 로그를 더 자세하게 확인 가능
+                .andExpect(status().isBadRequest()) //Status 상태코드 검증
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("상품 판매상태는 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("신규 상품을 등록할 때 상품 이름은 필수값이다.")
+    @Test
+    void createProductWithOutName() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .price(4000)
+                .build();
+
+        // when // then
+        // PostMapping의 경우 바디에 데이터를 넣어서 보내는데 이경우 JSON 형태로 보내므로 request 객체를 직렬화/역직렬화를 도와주는 ObjectMapper가 필요함.
+        mockMvc.perform(
+                        post("/api/v1/products/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print()) // 요청에 대한 로그를 더 자세하게 확인 가능
+                .andExpect(status().isBadRequest()) //Status 상태코드 검증
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("상품 이름은 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("신규 상품을 등록할 때 상품 가격은 양수이다.") // -> price 0 이 경계값
+    @Test
+    void createProductWithOutZeroPrice() throws Exception {
+        // given
+        ProductCreateRequest request = ProductCreateRequest.builder()
+                .type(ProductType.HANDMADE)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("아메리카노")
+                .price(0)
+                .build();
+
+        // when // then
+        // PostMapping의 경우 바디에 데이터를 넣어서 보내는데 이경우 JSON 형태로 보내므로 request 객체를 직렬화/역직렬화를 도와주는 ObjectMapper가 필요함.
+        mockMvc.perform(
+                        post("/api/v1/products/new")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print()) // 요청에 대한 로그를 더 자세하게 확인 가능
+                .andExpect(status().isBadRequest()) //Status 상태코드 검증
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("상품 가격은 양수여야 합니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
     }
 }
