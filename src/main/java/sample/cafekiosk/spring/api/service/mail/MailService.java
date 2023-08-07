31db60550b4810834a25329ff15c2eb45ef0fd83
jonghyunhub <chuwhdgus@gmail.com>
2023-08-08 00:18:36 +0900

    feat : 주문통계서비스 sendOrderStatisticsMail() 메서드 작성

--- src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/mail/MailService.java
@@ -1,4 +1,12 @@
 package sample.cafekiosk.spring.api.service.mail;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class MailService {
+    public boolean sendMail(String fromEmail, String toEmail, String subject, String content) {
+        return false;
+    }
 }
