7af263dfc88a71336df473990a724749d7bb29cc
jonghyunhub <chuwhdgus@gmail.com>
2023-08-08 01:25:00 +0900

    test: OrderStatisticsServiceTest 클래스 추가

--- src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
+++ src/main/java/sample/cafekiosk/spring/client/mail/MailSendClient.java
@@ -1,10 +1,13 @@
 package sample.cafekiosk.spring.client.mail;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class MailSendClient {
     public boolean sendMail(String fromEmail, String toEmail, String subject, String content) {
-
+        log.info("메일 전송");
+        return true;
     }
 }
