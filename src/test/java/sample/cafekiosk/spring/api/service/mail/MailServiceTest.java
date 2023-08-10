780177bda6239fbfe2bffb17c1659467db08166d
jonghyunhub <chuwhdgus@gmail.com>
2023-08-11 02:58:19 +0900

    refact : BDD Mockito 사용

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,9 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
@@ -33,8 +31,11 @@ class MailServiceTest {
     @Test
     void sendMail() {
         // given
-//        when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+//        Mockito.when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
 //                .thenReturn(true);
+        BDDMockito.given(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
+
 
         doReturn(true)
                 .when(mailSendClient)
